e36ceb7369073da3b952e2715bc7279f7776da0a
Ty-Lucas Kelley <tylucaskelley@gmail.com>
2016-05-16 08:42:21 -0400

    fix ruby & python whitespace error

--- bin/scripts/python.bash
+++ bin/scripts/python.bash
@@ -23,9 +23,9 @@ pyenv update
 PYTHON2_VERSION="$(pyenv install -l | grep -e '2.[0-9].[0-9]' | grep -v - | tail -1)"
 PYTHON3_VERSION="$(pyenv install -l | grep -e '3.[0-9].[0-9]' | grep -v - | tail -1)"
 
-pyenv install "$PYTHON2_VERSION"
-pyenv install "$PYTHON3_VERSION"
-pyenv global "$PYTHON2_VERSION" "$PYTHON3_VERSION"
+pyenv install $PYTHON2_VERSION
+pyenv install $PYTHON3_VERSION
+pyenv global $PYTHON2_VERSION $PYTHON3_VERSION
 
 for p in "${PACKAGES[@]}"; do
     pip install "$p"
